b2ee2dbc3f4b074b8f48684c2b22e0fbc6becef7
Vijay Hareesh <vijayhareesh300@gmail.com>
2023-11-26 14:34:12 +0530

    hello

---
+++ problem soloving .js
@@ -0,0 +1,42 @@
+// write a program area of right angle traiangle
+var base=60;
+var height=50
+
+var t=0.5*60*50
+console.log(t)
+
+
+// write aprogram distance b/w two points?
+var x1 =50
+var y1=30
+
+var x2=100
+var y2=60 
+
+ var m=(x2-x1)
+ var n=(y2-y1)
+ 
+ var l= m**2+n**2
+ console.log(l**0.5)
+
+ // coins
+var m=10
+var n=5
+var o=2
+var p=1
+
+var a=100
+var b=50
+var c=20
+var d=10
+
+var amount=(10*100)+(5*50)+(2*20)+(1*10)
+
+console.log(amount)
+ 
+
+
+//  write a program to extract last digit of a number
+var num=8765
+var r=num%10
+console.log(r)
